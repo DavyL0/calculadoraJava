5ad9272d3a11f2ca44afb7e01dfed74298633145
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2024-10-19 14:58:21 -0300

    Atualização do Projeto
    - Adição e implementação do Arquivo Css para o javaFx
    - Mudança nos botões 
    
    proximos passos: 
    - criar classes e dar funções aos botões

---
+++ calculadora/src/main/java/org/example/calculadora/HelloApplication.java
@@ -0,0 +1,25 @@
+package org.example.calculadora;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 350, 540);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("pixel.css")).toExternalForm());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
