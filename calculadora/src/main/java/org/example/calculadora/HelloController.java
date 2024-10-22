e6d20ab2af9815552ccad0d6fdb9ac297dd61ee0
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2024-10-22 14:03:27 -0300

    Atualização 0.8
    Frontend quase pronto.
    -cores
    -fonte
    -função dos botões
    
    proximo passos:
    -configurar o funcionamento da calculadora

--- calculadora/src/main/java/org/example/calculadora/HelloController.java
+++ calculadora/src/main/java/org/example/calculadora/HelloController.java
@@ -1,14 +1,38 @@
 package org.example.calculadora;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label label;
+    @FXML
+    private void handleButtonAction(ActionEvent event){
+        // Obtém o botão que foi clicado
+        Button clickedButton = (Button) event.getSource();
+        if (clickedButton.getText().equals("C")){
+            label.setText("");
+        }else {
+            // Define o texto do label com o texto do botão
+            String currentText = label.getText();  // Texto atual do Label
+            String newText = clickedButton.getText();  // Texto do botão clicado
+
+            // Define o texto concatenado no Label
+            label.setText(currentText + " " + newText);
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
