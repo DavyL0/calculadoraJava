4d7afa6af9432919e44dea458bfca4238bc7fc73
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2024-10-29 20:27:09 -0300

    Melhoria no frontend
    adição de background que se torna mais proximo do modelo

--- calculadora/src/main/java/org/example/calculadora/Application.java
+++ calculadora/src/main/java/org/example/calculadora/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("calculator-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 350, 537);
+        Scene scene = new Scene(fxmlLoader.load(), 350, 499);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("pixel.css")).toExternalForm());
         stage.setTitle("Calculadora 8bit");
         stage.setScene(scene);
